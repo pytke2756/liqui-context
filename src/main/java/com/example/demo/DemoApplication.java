282d2f5c6924731ff75acf351067fb2c76d5959f
pytke2756 <ibranyilevente@gmail.com>
2024-03-26 19:42:01 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String sql = "SELECT * FROM important";
+
+
+		jdbcTemplate.queryForList(sql);
+		String rows = jdbcTemplate.queryForList(sql).toString();
+		System.out.println(rows);
+	}
+}
